3ee6535bf1cd697efcc421911320b2af76d5068e
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-03-04 21:52:28 +0200

    (utils-url): Remove obsolete contstant

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,12 +1,11 @@
 /* Templated url methods */
 
 export const URL_PARAM_REGEXP = /(:[^\/]+)/g;
-export const URL_PARAM_EXTRACT = /(:[^\/]+)/;
 const urlToRegexp = url => RegExp(`^${url.replace(URL_PARAM_REGEXP, '([^/]+)')}$`);
 
 export const isMatchingPath = (template, url) => {
   template = urlToRegexp(template);
-  console.log('Match ' + template + ' vs ' + url + ' = ' + template.test(url));
+  // console.log('Match ' + template + ' vs ' + url + ' = ' + template.test(url));
   return template.test(url);
 };
 
