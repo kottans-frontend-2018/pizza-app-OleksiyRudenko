7d66b9b6385139dd6e85428da21d3b216b5d729e
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-04-14 10:49:07 +0300

    (pizza editor): Make PizzaEditor support children state binding

--- src/components/PizzaEditor/PizzaEditor.js
+++ src/components/PizzaEditor/PizzaEditor.js
@@ -1,5 +1,9 @@
 import Component from '../../Component';
+import PizzaParams from '../PizzaParams';
+import PizzaCanvas from '../PizzaCanvas';
 import * as dom from '../../utils/dom.js';
+import {PizzaImagery} from "../../services/PizzaImagery";
+import './style.css';
 
 /**
  * Class representing particular order.
@@ -8,24 +12,59 @@ export default class PizzaEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: this.props.params.id,
+    };
+    this.prerendered = this.preRender();
+    this.children = {
+      pizzaCanvas: new PizzaCanvas({
+        host: this.prerendered.canvas,
+        size: 60,
+      }),
+      pizzaConstructor: new PizzaParams({
+        host: this.prerendered.params,
+      }),
     };
   }
 
+  updateChildrenState() {
+    this.children.pizzaConstructor.updateState();
+    this.children.pizzaCanvas.updateState();
+  }
+
+  /**
+   * Render view
+   * @returns {HTMLElement}
+   */
   render() {
+    return this.prerendered.fragment;
+  }
+
+  /**
+   * PreRenders component view.
+   * @returns {object} {fragment, elements...}
+   */
+  preRender() {
+    console.log(this.name + '.preRender() in having ', this.state);
     const container = dom.createElement({
       tag: 'div',
-      id: 'pizzaeditor-container',
+      id: 'pizzaEditor-container',
       classList: ['row-wrap'],
     });
 
-    container.innerHTML = `
-      <h2>Pizza</h2>
-      <a href="#/order/${Math.floor(Math.random()*100)+1}">Random order</a>
-      <hr/>
-      <a href="#/">Home</a>
-      <a href="#/orders">Orders</a>
-      <a href="#/${Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}">Random link</a>`;
-    return container;
+    const canvas = dom.createElement('canvas');
+    const canvasContainer = dom.createElement({
+      tag: 'div',
+      classList: ['pizzaEditor-canvas'],
+    });
+    canvasContainer.appendChild(canvas);
+    const params = dom.createElement('div');
+
+    dom.setChildren(container, [ canvasContainer, params ]);
+
+    return {
+      fragment: container,
+      canvas: canvas,
+      params: params,
+    };
   }
+
 }
