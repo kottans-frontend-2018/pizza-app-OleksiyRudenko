fcf5f12e3e6549508c809a39e4a288f35fcb2603
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-04-15 23:04:20 +0300

    (pizza editor): Watch state change and manage drawing update

--- src/components/PizzaEditor/PizzaEditor.js
+++ src/components/PizzaEditor/PizzaEditor.js
@@ -2,7 +2,6 @@ import Component from '../../Component';
 import PizzaParams from '../PizzaParams';
 import PizzaCanvas from '../PizzaCanvas';
 import * as dom from '../../utils/dom.js';
-import {PizzaImagery} from "../../services/PizzaImagery";
 import './style.css';
 
 /**
@@ -13,18 +12,39 @@ export default class PizzaEditor extends Component {
     super(props);
     this.state = {
     };
+    dom.bindHandlers(this, 'onParamsChange');
     this.prerendered = this.preRender();
+    const pizzaInitialState = {
+      size: 60,
+      ingredients: {},
+      tags: {},
+      name: '',
+      description: '',
+    };
     this.children = {
       pizzaCanvas: new PizzaCanvas({
         host: this.prerendered.canvas,
-        size: 60,
+        state: pizzaInitialState,
       }),
       pizzaConstructor: new PizzaParams({
         host: this.prerendered.params,
+        onParamsChange: this.onParamsChange,
+        state: pizzaInitialState,
       }),
     };
   }
 
+  /**
+   * Updates pizzaCanvas when pizza params changed within pizzaConstructor
+   * @param {object} state
+   */
+  onParamsChange(state) {
+    this.children.pizzaCanvas.updateState(state);
+  }
+
+  /**
+   * Called within Component workflow
+   */
   updateChildrenState() {
     this.children.pizzaConstructor.updateState();
     this.children.pizzaCanvas.updateState();
