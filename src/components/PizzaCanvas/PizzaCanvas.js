f9db39600c6ec1e39bab28a8344bcefd7b1b8c29
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2018-04-15 23:02:55 +0300

    (pizza editor): Draw pizza with evenly distributed ingredients

--- src/components/PizzaCanvas/PizzaCanvas.js
+++ src/components/PizzaCanvas/PizzaCanvas.js
@@ -1,28 +1,126 @@
 import Component from '../../Component';
-import * as dom from '../../utils/dom.js';
+import {PizzaImagery} from "../../services/PizzaImagery";
+import {CANVAS} from './settings';
 
 /**
- * Class representing particular order.
- * host is CANVAS element
+ * Class representing pizza constructed image.
+ * host is a <canvas> element
  */
 export default class PizzaCanvas extends Component {
   constructor(props) {
     super(props);
     this.ctx = this.host.getContext("2d");
-    this.canvasWidth = this.canvasHeight = 320;
 
-    this.host.width = this.canvasWidth;
-    this.host.height = this.canvasHeight;
+    this.host.width = CANVAS.px.width;
+    this.host.height = CANVAS.px.height;
   }
 
   /**
    * Render view
-   * @returns {HTMLElement}
+   * @returns {HTMLElement|string}
    */
   render() {
-    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-    this.ctx.fillRect(10,10,150,150);
+    this.ctx.clearRect(0, 0, CANVAS.px.width, CANVAS.px.height);
+    // this.ctx.fillRect(10,10,150,150);
+    console.log(this.name + '.render() state', this.state);
+    this.drawPizzaBase();
+    this.drawIngredients();
     return '';
   }
 
+  /**
+   * Draws ingredients
+   */
+  drawIngredients() {
+    const area = CANVAS.areas[this.state.size];
+    console.log(this.name + '.drawIngredients() area', area);
+
+    const ingredientsCount = Object.keys(this.state.ingredients).length;
+
+    const nodes = this._distributeNodesEvenly(area.nodes, ingredientsCount);
+
+    Object.keys(this.state.ingredients).forEach((item, idx) => {
+      PizzaImagery.getImage(item)
+        .then(image => {
+          const coordSet = nodes[idx];
+          for(let i = coordSet.length - 1; i >= 0; i--) {
+            const polar = coordSet[i];
+            this._drawImage(image, polar.r, polar.asimuth, CANVAS.ingredient.width, CANVAS.ingredient.height);
+          }
+        })
+        .catch(rejection => console.log(this.name + '.drawIngredients() REJECTED', rejection));
+    });
+  }
+
+  /**
+   * Distributes array of nodes evenly across count groups
+   * @param {array} nodes [{r,asimuth}...]
+   * @param {number} count
+   * @returns {array} [[{r,asimuth}...]...]
+   * @private
+   */
+  _distributeNodesEvenly(nodes, count) {
+    const groupedNodes = [];
+    const len = nodes.length;
+    const times = Math.ceil(len / count);
+    for (let i = 0; i < count; i++) {
+      groupedNodes[i] = [];
+      for (let j = 0; j < times; j++) {
+        if (j*count+i < len) {
+          groupedNodes[i].push(nodes[j*count+i]);
+        }
+      }
+    }
+    return groupedNodes;
+  }
+
+  /**
+   * Draws scaled pizza base
+   */
+  drawPizzaBase() {
+    PizzaImagery.getImage('pizza')
+      .then(image => {
+        const pxDimension = this.state.size * CANVAS.pxInch;
+        image.width = pxDimension;
+        image.height = pxDimension;
+        console.log(this.name + '.drawPizzaBase()', image);
+        this._drawImage(image, 0, 0, pxDimension, pxDimension);
+        /* const img = new Image(pxDimension, pxDimension);
+        img.src = image.src;
+        img.onload = () => this.ctx.drawImage(img, 0, 0); */
+      })
+      .catch(rejection => console.log(this.name + '.drawPizzaBase() REJECTED', rejection));
+  }
+
+  /**
+   * Draws an image where its center is in polar coordinates (r,azimuth)
+   * @param {HTMLimageElement} image
+   * @param {number} r
+   * @param {number} azimuth
+   * @param {number} width
+   * @param {number} height
+   * @private
+   */
+  _drawImage(image, r, azimuth, width=0, height=0) {
+    if (!width) width = image.width;
+    if (!height) height = image.height;
+    const dx = -Math.round(width / 2);
+    const dy = -Math.round(height / 2);
+    const realCoordinates = this._polar2carthesian(r, azimuth);
+    this.ctx.drawImage(image, realCoordinates.x + dx, realCoordinates.y + dy, width, height);
+  }
+
+  /**
+   * Converts polar coordinates
+   * @param {number} r
+   * @param {number} azimuth
+   * @returns {object} {x,y} ready to draw at, based on CANVAS settings
+   * @private
+   */
+  _polar2carthesian(r, azimuth) {
+    const radians = azimuth * Math.PI / 180;
+    const x = Math.round(r * Math.cos(radians) + CANVAS.center.x);
+    const y = Math.round(-r * Math.sin(radians) + CANVAS.center.y);
+    return { x, y };
+  }
 }
---
+++ src/components/PizzaCanvas/settings.js
@@ -0,0 +1,107 @@
+export const CANVAS = {
+  px: {
+    width: 320,
+    height: 320,
+  },
+  inch: {
+    width: 60,
+    height: 60,
+  },
+  pxInch: 0, // pixels per inch
+  ingredient: {
+    width: 20,
+    height: 20,
+  },
+  areas: {
+    30: {
+      maxRadius: 0, // max px radius allowed
+      nodes: [], // nodes list of {r, asimuth}...
+      maxIngredients: 14, // max ingredients allowed
+    },
+    45: {
+      maxRadius: 0,
+      nodes: [],
+      maxIngredients: 14,
+    },
+    60: {
+      maxRadius: 0,
+      nodes: [],
+      maxIngredients: 14,
+    },
+  }
+};
+CANVAS['pxInch'] = Math.floor(CANVAS.px.width / CANVAS.inch.width);
+CANVAS.center = {
+  x: Math.floor(CANVAS.px.width / 2),
+  y: Math.floor(CANVAS.px.height / 2),
+};
+Object.keys(CANVAS.areas).forEach(r => {
+  CANVAS.areas[r].maxRadius = Math.round(r * CANVAS.pxInch / 2 - r / 3);
+});
+
+/* Build nodes (placeholders) for every radius at step 10px
+ At r=150 degreeStep=10deg
+ At r=5 degreeStep=45deg
+ */
+const targetR = CANVAS.pxInch * CANVAS.inch.height;
+const radii = Object.keys(CANVAS.areas);
+const nodes = {
+  30: [],
+  45: [],
+  60: [],
+};
+
+for(let r = 10; r < targetR; r += 10) {
+  const itemsOnCircle = Math.floor(Math.PI * 2 * r / (CANVAS.ingredient.width * 1.6));
+  const degStep = 360 / itemsOnCircle;
+  for(let deg = 0; deg < 360; deg += degStep * 0.7) {
+    const pair = {
+      r: r + (-Math.random() * 4 + 3),
+      asimuth: deg + (-Math.random() * 15 + 7.5),
+    };
+    radii.forEach(inches => {
+      if (CANVAS.areas[inches].maxRadius > r) {
+        nodes[inches].push(pair);
+      }
+    });
+  }
+}
+
+// copy nodes to CANVAS
+radii.forEach(inches => {
+  CANVAS.areas[inches].nodes = nodes[inches];
+});
+
+/* For each area distribute nodes evenly across maxIngredients node subsets
+   When drawing choose subset wisely.
+   Given 12 subsets use those dep on ingredients count:
+   1: 0
+   2: 0, 6
+   3: 0, 4, 8
+   4: 0, 3, 6, 9
+   5: 0, 2, 5, 7, 10 -- i.e. every round(12/n)
+*/
+/*
+radii.forEach(inches => {
+  const maxIngredients = CANVAS.areas[inches].maxIngredients;
+  const len = nodes[inches].length;
+  const times = Math.ceil(len / maxIngredients);
+  for (let i = 0; i < maxIngredients; i++) {
+    CANVAS.areas[inches].nodes[i] = [];
+    for (let j = 0; j < times; j++) {
+      if (j*maxIngredients+i < len) {
+        CANVAS.areas[inches].nodes[i].push(nodes[inches][j*maxIngredients+i]);
+      }
+    }
+  }
+}); */
+
+/* In the end of the day we draw
+   per each .areas[]
+   .areas[].maxIngredients ingredients
+   each .areas[].ingredientInstancesPerArea times
+   at .areas[].nodes[]
+   max .areas[].length sprites
+ */
+
+// console.log('CANVAS SETTINGS', CANVAS);
